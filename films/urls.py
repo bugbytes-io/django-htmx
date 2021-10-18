6dce71147f29b4be2de50d0243560aad9bf8ae44
bugbytes-io <bugbytes.io@gmail.com>
2021-10-18 21:19:19 +0100

    Initial commit

---
+++ films/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from films import views
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [
+    path('index/', views.IndexView.as_view(), name='index'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path("register/", views.RegisterView.as_view(), name="register")
+]
---
+++ films/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.views.generic import FormView, TemplateView
+
+from films.forms import RegisterForm
+
+# Create your views here.
+class IndexView(TemplateView):
+    template_name = 'index.html'
+    
+class Login(LoginView):
+    template_name = 'registration/login.html'
+
+class RegisterView(FormView):
+    form_class = RegisterForm
+    template_name = 'registration/register.html'
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        form.save()  # save the user
+        return super().form_valid(form)
