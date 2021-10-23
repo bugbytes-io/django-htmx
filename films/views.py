a847153f0a842ecbf89c6a449d80903c6cd7a3f7
bugbytes-io <bugbytes.io@gmail.com>
2021-10-23 13:46:50 +0100

    htmx1

--- films/views.py
+++ films/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
+from django.http import HttpResponse
 from django.views.generic import FormView, TemplateView
 
 from films.forms import RegisterForm
@@ -20,3 +22,10 @@ class RegisterView(FormView):
     def form_valid(self, form):
         form.save()  # save the user
         return super().form_valid(form)
+
+def check_username(request):
+    username = request.POST.get('username')
+    if get_user_model().objects.filter(username=username).exists():
+        return HttpResponse("This username already exists")
+    else:
+        return HttpResponse("")
