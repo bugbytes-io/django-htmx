d78b4005ff9efa57f7ca84d2d136c77c0395002d
bugbytes-io <bugbytes.io@gmail.com>
2021-10-29 09:49:00 +0100

    Starter Code

--- films/views.py
+++ films/views.py
@@ -1,7 +1,9 @@
+from django.http.response import HttpResponse, HttpResponsePermanentRedirect
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from django.views.generic import FormView, TemplateView
+from django.contrib.auth import get_user_model
 
 from films.forms import RegisterForm
 
