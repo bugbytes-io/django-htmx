b77acab028784bde05e2ba2a03597593c4faa17a
bugbytes-io <bugbytes.io@gmail.com>
2021-11-01 19:25:09 +0000

    Add-film functionality added

--- films/models.py
+++ films/models.py
@@ -3,3 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+class Film(models.Model):
+    name = models.CharField(max_length=128)
+    users = models.ManyToManyField(User, related_name='films')
