f6c4c4f560564eab214e95aff12d6a8b3deaffc6
bugbytes-io <bugbytes.io@gmail.com>
2021-11-07 15:54:04 +0000

    Add missing unique constraint

--- films/models.py
+++ films/models.py
@@ -5,5 +5,5 @@ class User(AbstractUser):
     pass
 
 class Film(models.Model):
-    name = models.CharField(max_length=128)
+    name = models.CharField(max_length=128, unique=True)
     users = models.ManyToManyField(User, related_name='films')
